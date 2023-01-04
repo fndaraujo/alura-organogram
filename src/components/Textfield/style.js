a38cacf80c6a2447449a75f41b49d847ff563bf3
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2023-01-04 20:20:03 -0300

    Create stylesheet for component textfield

---
+++ src/components/Textfield/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const StyledTextfield = styled.div`
+  margin: 24px 0;
+
+  label {
+    display: block;
+    margin-bottom: 8px;
+    font-size: 24px;
+    text-transform: capitalize;
+  }
+  input {
+    background-color: white;
+    box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.08);
+    width: 100%;
+    border: none;
+    font-size: 24px;
+    padding: 24px;
+  }
+`
