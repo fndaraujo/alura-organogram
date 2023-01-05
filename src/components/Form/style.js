9d6c6018e6be7c3c7616efe6645dcb79c9d4e75d
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2023-01-04 21:37:55 -0300

    Create stylesheet for component form

---
+++ src/components/Form/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const StyledForm = styled.section`
+  display: flex;
+  justify-content: center;
+  margin: 80px 0;
+
+  h2 {
+    font-size: 32px;
+    font-weight: bold;
+  }
+
+  form {
+    max-width: 80%;
+    background-color: #f2f2f2;
+    border-radius: 20px;
+    padding: 36px 64px;
+    box-shadow: 8px 8px 16px rgba(0, 0, 0, 0.16);
+  }
+`
