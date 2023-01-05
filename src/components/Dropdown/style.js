e341555867756a173e7a1a91a93d2f284c66ef69
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2023-01-05 15:35:02 -0300

    Create stylesheet for component drop down selector

---
+++ src/components/Dropdown/style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const StyledDropdown = styled.div`
+  label {
+    display: block;
+    margin-bottom: 8px;
+    font-size: 24px;
+    text-transform: capitalize;
+  }
+
+  select {
+    background-color: white;
+    box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.08);
+    width: 100%;
+    border: none;
+    font-size: 24px;
+    padding: 24px;
+  }
+`
