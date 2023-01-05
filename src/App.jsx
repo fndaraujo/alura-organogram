a96e76ace7d79eb77ed2fcdc236011703d7cf45c
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2023-01-04 21:39:16 -0300

    Add component form in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Banner } from '@/components/Banner'
+import { Form } from './components/Form'
 import { GlobalStyle } from '@/styles/globalstyle'
 
 export const App = () => (
   <>
     <GlobalStyle />
     <Banner />
+    <Form />
   </>
 )
