3230b8e47de6cc5269ee7b0bc047eb2c2d611c21
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2023-01-05 16:20:41 -0300

    Add html required field validation parameters in component form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -21,10 +21,18 @@ export const Form = () => (
   <StyledForm>
     <form onSubmit={handleSubmit}>
       <h2>Fill in the fields to create a contributor card</h2>
-      <Textfield label={'name'} placeholder={'Enter name...'} />
-      <Textfield label={'role'} placeholder={'Enter role...'} />
+      <Textfield
+        isRequired={true}
+        label={'name'}
+        placeholder={'Enter name...'}
+      />
+      <Textfield
+        isRequired={true}
+        label={'role'}
+        placeholder={'Enter role...'}
+      />
       <Textfield label={'image'} placeholder={'Enter image url...'} />
-      <Dropdown label={'team'} list={teams} />
+      <Dropdown isRequired={true} label={'team'} list={teams} />
       <Button>Create card</Button>
     </form>
   </StyledForm>
