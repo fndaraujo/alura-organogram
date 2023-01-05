b51e2f65afc1b7cd12a1dea51b6f390f2670041a
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2023-01-05 15:40:34 -0300

    Create stylesheet for component button

---
+++ src/components/Button/style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const StyledButton = styled.button`
+  background-color: #6278f7;
+  border: none;
+  border-radius: 10px;
+  font-weight: 700;
+  font-size: 18px;
+  padding: 32px;
+  cursor: pointer;
+  color: white;
+  margin: 32px 0;
+
+  :hover {
+    color: #95ffd4;
+  }
+`
