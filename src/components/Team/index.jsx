004e88f91e015f800249e77bea8d180ecf5f4c78
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2023-01-06 03:57:44 -0300

    Make component team render only teams that have contributors

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -3,19 +3,24 @@ import { StyledTeam } from '@/components/Team/style'
 
 export const Team = ({ team, contributors }) => {
   return (
-    <StyledTeam primary={team.colors.primary} secundary={team.colors.secundary}>
-      <h3>{team.name}</h3>
-      <div className="contributor">
-        {contributors.map((contributor) => (
-          <Contributor
-            key={contributor.name}
-            name={contributor.name}
-            role={contributor.role}
-            image={contributor.image}
-            headerColor={team.colors.primary}
-          />
-        ))}
-      </div>
-    </StyledTeam>
+    contributors.length > 0 && (
+      <StyledTeam
+        primary={team.colors.primary}
+        secundary={team.colors.secundary}
+      >
+        <h3>{team.name}</h3>
+        <div className="contributor">
+          {contributors.map((contributor) => (
+            <Contributor
+              key={contributor.name}
+              name={contributor.name}
+              role={contributor.role}
+              image={contributor.image}
+              headerColor={team.colors.primary}
+            />
+          ))}
+        </div>
+      </StyledTeam>
+    )
   )
 }
