e12f59b8a835db2138748bc5e905a8f39b586d5b
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2023-01-06 01:17:13 -0300

    Create constant teams to store a list of team information

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,57 @@ import { Form } from '@/components/Form'
 import { GlobalStyle } from '@/styles/globalstyle'
 
 export const App = () => {
+  const teams = [
+    {
+      name: 'Programming',
+      colors: {
+        primary: '#57c278',
+        secundary: '#d9f7e9',
+      },
+    },
+    {
+      name: 'Front-End',
+      colors: {
+        primary: '#82cffa',
+        secundary: '#e8f8ff',
+      },
+    },
+    {
+      name: 'Data Science',
+      colors: {
+        primary: '#a6d157',
+        secundary: '#f0f8e2',
+      },
+    },
+    {
+      name: 'Devops',
+      colors: {
+        primary: '#e06b69',
+        secundary: '#fde7e8',
+      },
+    },
+    {
+      name: 'UX & Design',
+      colors: {
+        primary: '#db6ebf',
+        secundary: '#fae9f5',
+      },
+    },
+    {
+      name: 'Mobile',
+      colors: {
+        primary: '#ffba05',
+        secundary: '#fff5d9',
+      },
+    },
+    {
+      name: 'Inovation & Management',
+      colors: {
+        primary: '#ff8a29',
+        secundary: '#ffeedf',
+      },
+    },
+  ]
   const [contributors, setContributors] = useState([])
   const handleOnRecord = (contributor) =>
     setContributors(...contributors, contributor)
