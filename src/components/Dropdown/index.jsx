d23e139a6641d2cac36b8dd4f12ec22d9edb8b41
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2023-01-05 22:05:11 -0300

    Make component drop down selector handle input state

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,9 +1,13 @@
 import { StyledDropdown } from '@/components/Dropdown/style'
 
-export const Dropdown = ({ isRequired, label, list }) => (
+export const Dropdown = ({ isRequired, label, list, value, setValue }) => (
   <StyledDropdown>
     <label>{label}</label>
-    <select required={isRequired}>
+    <select
+      required={isRequired}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    >
       {list.map((item) => (
         <option key={item}>{item}</option>
       ))}
