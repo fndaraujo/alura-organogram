3e0b2373e769893f80be6c245147b450bb3f6ec8
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2023-01-06 01:13:50 -0300

    Create stylesheet for component team

---
+++ src/components/Team/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const StyledTeam = styled.section`
+  text-align: center;
+  padding: 32px;
+  background-color: ${(props) => props.secundary || 'magenta'};
+
+  h3 {
+    font-size: 32px;
+    border-bottom: 4px solid ${(props) => props.primary || 'magenta'};
+    display: inline-block;
+    padding-bottom: 8px;
+  }
+`
