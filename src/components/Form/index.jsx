a33fbde848f1790028d32d1e88330e9630c67bbf
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2023-01-05 22:08:22 -0300

    Make component form pass input values in the input components

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,39 +1,61 @@
+import { useState } from 'react'
 import { Textfield } from '@/components/Textfield'
 import { Dropdown } from '@/components/Dropdown'
 import { Button } from '@/components/Button'
 import { StyledForm } from '@/components/Form/style'
 
-const teams = [
-  'Programming',
-  'Front-End',
-  'Data Science',
-  'Devps',
-  'UX & Design',
-  'Mobile',
-  'Inovation & Management',
-]
+export const Form = () => {
+  const [name, setName] = useState('')
+  const [role, setRole] = useState('')
+  const [image, setImage] = useState('')
+  const [team, setTeam] = useState('')
+  const teams = [
+    'Programming',
+    'Front-End',
+    'Data Science',
+    'Devps',
+    'UX & Design',
+    'Mobile',
+    'Inovation & Management',
+  ]
 
-const handleSubmit = (e) => {
-  e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <StyledForm>
+      <form onSubmit={handleSubmit}>
+        <h2>Fill in the fields to create a contributor card</h2>
+        <Textfield
+          isRequired={true}
+          label={'name'}
+          placeholder={'Enter name...'}
+          value={name}
+          setValue={setName}
+        />
+        <Textfield
+          isRequired={true}
+          label={'role'}
+          placeholder={'Enter role...'}
+          value={role}
+          setValue={setRole}
+        />
+        <Textfield
+          label={'image'}
+          placeholder={'Enter image url...'}
+          value={image}
+          setValue={setImage}
+        />
+        <Dropdown
+          isRequired={true}
+          label={'team'}
+          list={teams}
+          value={team}
+          setValue={setTeam}
+        />
+        <Button>Create card</Button>
+      </form>
+    </StyledForm>
+  )
 }
-
-export const Form = () => (
-  <StyledForm>
-    <form onSubmit={handleSubmit}>
-      <h2>Fill in the fields to create a contributor card</h2>
-      <Textfield
-        isRequired={true}
-        label={'name'}
-        placeholder={'Enter name...'}
-      />
-      <Textfield
-        isRequired={true}
-        label={'role'}
-        placeholder={'Enter role...'}
-      />
-      <Textfield label={'image'} placeholder={'Enter image url...'} />
-      <Dropdown isRequired={true} label={'team'} list={teams} />
-      <Button>Create card</Button>
-    </form>
-  </StyledForm>
-)
