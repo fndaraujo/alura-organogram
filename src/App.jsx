80bef749f192a154aeacabba6467c5320c827eb7
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2023-01-05 23:07:15 -0300

    Fix form relative path to absolute path import in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Banner } from '@/components/Banner'
-import { Form } from './components/Form'
+import { Form } from '@/components/Form'
 import { GlobalStyle } from '@/styles/globalstyle'
 
 export const App = () => {
